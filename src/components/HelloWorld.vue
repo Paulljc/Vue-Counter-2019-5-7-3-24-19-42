a03a464795237b5e4b68e522610c374c56244b1f
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-19 15:44:11 +0800

    [LJC]:finish counter

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <button @click="decrease"> - </button>
+      <span class="number">{{ count }}</span>
+    <button @click="increase"> + </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods:{
+    decrease(){
+      this.count--;
+    },
+    increase(){
+      this.count++;
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.number {
+  margin: 10px;
+}
+</style>
